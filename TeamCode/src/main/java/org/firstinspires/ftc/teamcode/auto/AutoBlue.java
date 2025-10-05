4f24a753fb46bbc9c633a34353386a51f66a0572
Arm201001 <aniketrmenon@gmail.com>
2025-10-05 17:00:57 -0400

    adding motors(i think)
    wdyt

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/AutoBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/AutoBlue.java
@@ -1,5 +1,12 @@
 package org.firstinspires.ftc.teamcode.auto;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 public class AutoBlue {
+    public DcMotor rightBackMotor;
+    public DcMotor rightFrontMotor;
+    public DcMotor leftBackMotor;
+    public DcMotor leftFrontMotor;
+
 
 }
