e5c17aa7c4cc0bfc035c7c9eaaedcdcc70790e10
Arm201001 <aniketrmenon@gmail.com>
2025-10-05 17:07:35 -0400

    additions
    ok

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/AutoBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/AutoBlue.java
@@ -1,12 +1,39 @@
-package org.firstinspires.ftc.teamcode.auto;
+package org.firstinspires.ftc.teamcode.pedroPathing; // make sure this aligns with class location
 
-import com.qualcomm.robotcore.hardware.DcMotor;
-
-public class AutoBlue {
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.Path;
+import com.pedropathing.paths.PathChain;
+import com.pedropathing.util.Timer;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import  com.qualcomm.robotcore.eventloop.opmode.OpMode;
+@Autonomous(name = "BlueAuto")
+public class AutoBlue extends OpMode {
     public DcMotor rightBackMotor;
     public DcMotor rightFrontMotor;
     public DcMotor leftBackMotor;
     public DcMotor leftFrontMotor;
+    private Follower follower;
+    private Timer pathTimer, actionTimer, opmodeTimer;
+
+    private int pathState;
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void start() {
+
+    }
+    @Override
+    public void loop() {
+
+    }
+
+
 
 
 }
