4aa5aee2a8bf7a974c6816c997a47a24b223361b
sudeepnela537 <nelakuditysudeep@gmail.com>
2025-10-09 09:23:18 -0400

    10/9

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/AutoRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/AutoRed.java
@@ -1,4 +1,23 @@
 package org.firstinspires.ftc.teamcode.auto;
 
-public class AutoRed {
+import com.pedropathing.follower.Follower;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class AutoRed extends LinearOpMode {
+    //Declare all hardware here
+    
+    Follower follower;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        //Initialize hardware
+
+        waitForStart();
+
+        if(opModeIsActive()){
+            //all of autonomous code in here
+        }
+
+    }
 }
