06bde443232bbf835a1c55b5924915a435180065
Rohit Vankamamidi <rohitvankamamidi864@gmail.com>
2025-10-12 17:28:47 -0400

    Update robotHardware.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotHardware.java
@@ -1,26 +1,47 @@
 package org.firstinspires.ftc.teamcode;
 
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
-
 import com.qualcomm.hardware.limelightvision.Limelight3A;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 
-public class robotHardware {
+public class RobotHardware {
     private Limelight3A limelight;
-    Testbench bench = new TestBench();
-    public void calculateDistance(int pipeline) {
-        double distance;
-        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+    private Telemetry telemetry;
+    private TestBench bench;  // assuming you have a TestBench class
 
+    // Constructor
+    public RobotHardware(HardwareMap hardwareMap, Telemetry telemetry) {
+        this.telemetry = telemetry;
+        this.bench = new TestBench();
+
+        // Initialize the Limelight camera
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+    }
+
+    public void calculateDistance(int pipeline) {
+        // Switch to the correct Limelight pipeline
+        limelight.pipelineSwitch(pipeline);
+
+        // Get latest result from Limelight
+        LLResult result = limelight.getLatestResult();
+
+        // Get robot orientation (if TestBench provides it)
         YawPitchRollAngles orientation = bench.getOrientation();
 
-        limelight.pipelineSwitch(pipeline);
-        telemetry.addData("TA:", );
+        if (result != null && result.isValid()) {
+            double targetArea = result.getTa();  // Example: target area
+            double tx = result.getTx();          // Horizontal offset
+            double ty = result.getTy();          // Vertical offset
+
+            telemetry.addData("Target Area", targetArea);
+            telemetry.addData("Target X", tx);
+            telemetry.addData("Target Y", ty);
+        } else {
+            telemetry.addLine("No valid Limelight result.");
+        }
+
+        telemetry.update();
     }
 }
