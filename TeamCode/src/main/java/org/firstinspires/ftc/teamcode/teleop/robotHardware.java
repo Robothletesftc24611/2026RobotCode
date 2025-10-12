075378c10100f01ed701a4ae33ea09082817ae7c
Arm201001 <aniketrmenon@gmail.com>
2025-10-12 17:28:04 -0400

    Update robotHardware.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/robotHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/robotHardware.java
@@ -1,26 +1,36 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.teleop;
 
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
 
 import com.qualcomm.hardware.limelightvision.Limelight3A;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.hardware.limelightvision.LLResult;
+
 
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 
+@TeleOp
 public class robotHardware {
     private Limelight3A limelight;
     Testbench bench = new TestBench();
     public void calculateDistance(int pipeline) {
         double distance;
         limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        limelight.pipelineSwitch(2);
 
         YawPitchRollAngles orientation = bench.getOrientation();
 
         limelight.pipelineSwitch(pipeline);
-        telemetry.addData("TA:", );
+        telemetry.addData("TA:",);
+    }
+    @Override
+    public void start() {
+        limelight.start();
+    }
+
+    @Override
+    public void loop(){
+
     }
 }
