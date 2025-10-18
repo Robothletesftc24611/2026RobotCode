50ed8acda7327d18913c4da3bfb0f039e2482664
Rohit Vankamamidi <rohitvankamamidi864@gmail.com>
2025-10-18 09:45:42 -0400

    Update mainteleop.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/mainteleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/mainteleop.java
@@ -29,6 +29,8 @@
 
 package org.firstinspires.ftc.teamcode.teleop;
 
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.LLResultTypes;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
@@ -36,6 +38,8 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
+
+import java.util.List;
 
 /*
  * This file contains an example of a Linear "OpMode".
@@ -102,7 +106,7 @@ public class mainteleop extends LinearOpMode {
         turretServo = hardwareMap.get(CRServo.class, "turretServo");
 
         limelight = hardwareMap.get(Limelight3A.class, "limelight");
-
+        limelight.pipelineSwitch(1);
 
         // ########################################################################################
         // !!!            IMPORTANT Drive Information. Test your motor directions.            !!!!!
@@ -179,8 +183,39 @@ public class mainteleop extends LinearOpMode {
             backLeftDrive.setPower(backLeftPower);
             backRightDrive.setPower(backRightPower);
 
-            //turret code
-            limelight.pipelineSwitch(1);
+            // Turret Code
+            LLResult llResult = limelight.getLatestResult(); //get latest result of what the camera sees
+            List<LLResultTypes.FiducialResult> fiducials = llResult.getFiducialResults(); //sort them into a list
+
+
+            if (!fiducials.isEmpty()) {
+                int tagId = fiducials.get(0).getFiducialId(); //get the most recent reading from the list
+                double tx = llResult.getTx(); //get the x axis offset of the most recent reading to center the turret properly
+                double correctedTx = tx - LIMELIGHT_OFFSET;
+
+
+                telemetry.addData("AprilTag ID", tagId);  //display values so it is easier to debug
+                telemetry.addData("Raw tx", "%.2f", tx);
+                telemetry.addData("Corrected tx", "%.2f", correctedTx);
+
+
+                if (Math.abs(correctedTx) > DEADZONE) { //only spin if it is needed (for small changes (<1 degree) there is no point of spinning)
+                    double power = -correctedTx / 30.0; // make sure that the range for power is always -1 to 1
+                    power = Math.max(-MAX_POWER, Math.min(MAX_POWER, power)); //make sure power never goes over 0.3
+
+
+                    turretServo.setPower(power); //set power value to the servo
+                    telemetry.addData("Turret Status", "Spinning");
+                    telemetry.addData("Servo Power", "%.2f", power);
+                } else {
+                    turretServo.setPower(0.0); // Stop when centered
+                    telemetry.addData("Turret Status", "Centered");
+                }
+            } else {
+                turretServo.setPower(0.0); // Stop if no tag
+                telemetry.addData("AprilTag", "Not Found");
+                telemetry.addData("Turret Status", "Holding");
+            }
 
 
             //intake code
