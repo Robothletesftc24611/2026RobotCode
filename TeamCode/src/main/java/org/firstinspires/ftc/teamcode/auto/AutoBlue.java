c2246883b3f0ddce97a87e3f80be6ea71f6787b1
sudeepnela537 <nelakuditysudeep@gmail.com>
2025-11-14 21:45:58 -0500

    11/14 6 ball auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/AutoBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/AutoBlue.java
@@ -7,13 +7,14 @@ import com.pedropathing.paths.PathChain;
 import com.pedropathing.util.Timer;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import  com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
-
+@Disabled
 @Autonomous(name = "BlueAuto")
 public class AutoBlue extends OpMode {
     private DcMotor frontLeftDrive = null;
